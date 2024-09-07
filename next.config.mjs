06c159d0c02d5aa11e9a2b7cc0ab7f13733bf94d
Saxobiddin Mavlonov <mavlonovsoxobiddin@gmail.com>
2024-09-07 16:13:19 +0500

    O'zgartirilgan fayllarni qo‘shish

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: 'media.graphassets.com',
+				hostname: 'us-east-1-shared-usea1-02.graphassets.com',
 			},
 		],
 	},
